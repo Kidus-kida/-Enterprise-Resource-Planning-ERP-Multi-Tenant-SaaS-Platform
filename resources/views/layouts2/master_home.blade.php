98523dbaf99881ab6b56b2470eb89d162d211762
teka8 <tekadefar8@gmail.com>
2025-05-20 17:05:02 +0300

    update

--- resources/views/layouts2/master_home.blade.php
+++ resources/views/layouts2/master_home.blade.php
@@ -10,24 +10,24 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{asset('frontend/assets/img/favicon.png')}}" rel="icon">
-  <link href="{{asset('frontend/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+  @vite(['resources/frontend/assets/img/favicon.png'])
+  @vite(['resources/frontend/assets/img/apple-touch-icon.png'])
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="{{asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/aos/aos.css')}}" rel="stylesheet">
-  <link href="{{asset('frontend/assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
-  <link href="{{asset('frontend/assets/css/style.css')}}" rel="stylesheet">
+  @vite([
+    'resources/frontend/assets/vendor/bootstrap/css/bootstrap.min.css',
+    'resources/frontend/assets/vendor/icofont/icofont.min.css',
+    'resources/frontend/assets/vendor/boxicons/css/boxicons.min.css',
+    'resources/frontend/assets/vendor/animate.css/animate.min.css',
+    'resources/frontend/assets/vendor/venobox/venobox.css',
+    'resources/frontend/assets/vendor/owl.carousel/assets/owl.carousel.min.css',
+    'resources/frontend/assets/vendor/aos/aos.css',
+    'resources/frontend/assets/vendor/remixicon/remixicon.css',
+    'resources/frontend/assets/css/style.css'
+  ])
 
   <!-- =======================================================
   * Template Name: Company - v2.1.0
@@ -39,7 +39,6 @@
 
 <body>
 
- 
  <!-- ======= Header ======= -->
   @include('layouts.body.header')
  <!-- End Header -->
@@ -58,22 +57,20 @@
   @include('layouts.body.footer')
   <!-- End Footer -->
 
-
-
   <!-- Vendor JS Files -->
-  <script src="{{asset('frontend/assets/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/jquery-sticky/jquery.sticky.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/venobox/venobox.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
-  <script src="{{asset('frontend/assets/vendor/aos/aos.js')}}"></script>
-
-  <!-- Template Main JS File -->
-  <script src="{{asset('frontend/assets/js/main.js')}}"></script>
+  @vite([
+    'resources/frontend/assets/vendor/jquery/jquery.min.js',
+    'resources/frontend/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/frontend/assets/vendor/jquery.easing/jquery.easing.min.js',
+    'resources/frontend/assets/vendor/php-email-form/validate.js',
+    'resources/frontend/assets/vendor/jquery-sticky/jquery.sticky.js',
+    'resources/frontend/assets/vendor/isotope-layout/isotope.pkgd.min.js',
+    'resources/frontend/assets/vendor/venobox/venobox.min.js',
+    'resources/frontend/assets/vendor/waypoints/jquery.waypoints.min.js',
+    'resources/frontend/assets/vendor/owl.carousel/owl.carousel.min.js',
+    'resources/frontend/assets/vendor/aos/aos.js',
+    'resources/frontend/assets/js/main.js'
+  ])
 
 </body>
 
