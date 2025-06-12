9b44fd4d3cddf3e26cefb1e5803448dcbc14afe1
natnael0024 <nathanleg23@gmail.com>
2025-06-12 09:37:36 +0300

    commit

--- app/Listeners/AppMenuListener.php
+++ app/Listeners/AppMenuListener.php
@@ -31,9 +31,7 @@ class AppMenuListener
         $menu->add(
             Link::toRoute('dashboard', '<i class="la la-dashboard"></i> <span> ' . __('Dashboard') . '</span>')->setActive(route_is('dashboard'))
         );
-        $menu->add(
-            Link::toRoute('folders.index', '<i class="la la-folder"></i> <span> ' . __('File Management') . '</span>')->setActive(route_is('folders.index'))
-        );
+        
         $activeClass = route_is(["app.chat"]) ? "active" : "";
         $menu
             ->submenu(
@@ -105,6 +103,9 @@ class AppMenuListener
         );
         $menu->addIfCan('view-assets',
             Link::toRoute('assets.index', '<i class="la la-object-ungroup"></i> <span>' . __('Assets') . '</span>')->setActive(route_is('assets.index'))
+        );
+        $menu->add(
+            Link::toRoute('folders.index', '<i class="la la-folder"></i> <span> ' . __('File Management') . '</span>')->setActive(route_is('folders.index'))
         );
 
     }
