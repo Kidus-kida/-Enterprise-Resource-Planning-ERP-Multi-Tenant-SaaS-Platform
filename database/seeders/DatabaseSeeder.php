365a8784f4f302a3aaf3431a7ac48546cf70bad3
Emmanuel-Alem <emmaalem012@gmail.com>
2025-06-14 08:29:48 +0300

    role permission for sidebar award accessibility only for HR

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             WorldSeeder::class,
+            RolePermissionSeeder::class,
         ]);
     }
 }
