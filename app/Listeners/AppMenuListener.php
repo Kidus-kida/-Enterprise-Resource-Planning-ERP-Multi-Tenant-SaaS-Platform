ef82cc89bfa6e2b774129f6fedf75d56d17df296
Tekalegne Defar <tekadefar8@gmail.com>
2025-06-16 15:09:59 +0300

    Update AppMenuListener.php

--- app/Listeners/AppMenuListener.php
+++ app/Listeners/AppMenuListener.php
@@ -43,15 +43,7 @@ class AppMenuListener
                     ->addParentClass('submenu')
             );
 
-<<<<<<< middlewarePermission
-        if (auth()->user()->canAny([
-            'view-employees',
-            'view-attendances',
-            'view-departments',
-            'view-designations',
-            'view-holidays'
-        ])) {
-=======
+
         if (
             auth()->user()->canAny([
                 'view-employees',
@@ -61,7 +53,6 @@ class AppMenuListener
                 'view-holidays'
             ])
         ) {
->>>>>>> main
             $menu->html('<span>Employees</span>', ['class' => 'menu-title']);
             $activeClass = route_is(['employees.index', 'employees.list', 'departments.index', 'designations.index', 'holidays.*']) ? "active" : "";
             $menu
@@ -74,8 +65,7 @@ class AppMenuListener
                         ->addIfCan('view-departments', Link::toRoute('departments.index', __('Departments'))->addClass(route_is('departments.index') ? 'active' : ''))
                         ->addIfCan('view-designations', Link::toRoute('designations.index', __('Designations'))->addClass(route_is('designations.index') ? 'active' : ''))
                         ->addIfCan('view-holidays', Link::toRoute('holidays.index', __('Holidays'))->addClass(route_is('holidays.*') ? 'active' : ''))
-<<<<<<< middlewarePermission
-=======
+
                 );
         }
 
@@ -99,7 +89,7 @@ class AppMenuListener
                         ->addIfCan('manage-leaverequest', Link::toRoute('leavetypes.index', __('Leave Types '))->addClass(route_is(['leavetypes.index']) ? 'active' : ''))
                         ->addIfCan('manage-annual-leave', Link::toRoute('annual_leaves.index', __('Annual Leave '))->addClass(route_is(['annual_leaves.index']) ? 'active' : ''))
 
->>>>>>> main
+
                 );
         }
         //end leave requests
