18c71ce6b17d5439c28e915cb02a13774e21003e
teka8 <tekadefar8@gmail.com>
2025-11-20 14:14:14 +0300

    'update'

--- Modules/Project/resources/views/tasks/index.blade.php
+++ Modules/Project/resources/views/tasks/index.blade.php
@@ -193,11 +193,6 @@
                                     <div class="kanban-box">
                                         <div class="task-card-header d-flex justify-content-between align-items-start">
                                             <h6 class="task-title flex-grow-1 mb-0"><a href="{{ route('tasks.show', $task->id) }}">{{ $task->name }}</a></h6>
-                                            <div class="d-flex flex-wrap mt-2">
-                                                @foreach($task->labels as $label)
-                                                    <span class="badge me-1" style="background-color: {{ $label->color }}">{{ $label->name }}</span>
-                                                @endforeach
-                                            </div>
                                             <div class="dropdown kanban-task-action">
                                                 <a href="#" data-bs-toggle="dropdown">
                                                     <i class="fa-solid fa-ellipsis-vertical"></i>
@@ -217,6 +212,11 @@
                                                     </a>
                                                 </div>
                                             </div>
+                                        </div>
+                                        <div class="d-flex flex-wrap mt-2">
+                                            @foreach($task->labels as $label)
+                                                <span class="badge me-1" style="background-color: {{ $label->color }}">{{ $label->name }}</span>
+                                            @endforeach
                                         </div>
                                         <div class="task-card-body">
                                             <div class="task-priority-indicator" style="background-color: {{ $board->color ?? '#42a5f5' }};"></div>
