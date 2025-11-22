38f4226b7122432a1c81b79a8a1786bd0c945eb9
yeamft <143818404+yeamft@users.noreply.github.com>
2025-11-22 10:06:41 +0300

    Update create.blade.php

--- Modules/Crm/resources/views/leads/create.blade.php
+++ Modules/Crm/resources/views/leads/create.blade.php
@@ -1,6 +1,7 @@
 <form action="{{ route('leads.store') }}" method="POST">
     @csrf
-    <div class="row">
+   <div class="modal-body p-3">
+        <div class="row g-3">
         <div class="col-md-6">
             <div class="form-group">
                 <label>{{ __('Name') }} <span class="text-danger">*</span></label>
@@ -54,4 +55,5 @@
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Cancel') }}</button>
         <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
     </div>
+   </div>
 </form>
