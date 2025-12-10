d293248cf3e27f55989181310e4b974ce3fa91e5
AmanKifle <amankifle54@gmail.com>
2025-12-10 10:29:29 +0300

    add purchase module

---
+++ Modules/Purchase/app/Listeners/AppMenuListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Modules\Purchase\Listeners;
+
+use App\Events\AppMenuEvent;
+
+
+class AppMenuListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(AppMenuEvent $event): void
+    {
+        //
+    }
+}
--- app/Listeners/AppMenuListener.php
+++ app/Listeners/AppMenuListener.php
@@ -146,6 +146,20 @@ class AppMenuListener
             );
         }
 
+        // Purchase
+        if(auth()->user()->canAny(['view-taxes','view-expenses','view-estimates','view-invoices'])){
+            $activeClass = route_is(["taxes.*","expenses.*","estimates.*","invoices.*"]) ? "active" : "";
+            $menu->submenu(
+                Html::raw('<a href="#" class="' . $activeClass . '"><i class="la la-shopping-bag"></i><span>' . __("Purchase") . '</span><span class="menu-arrow"></span></a>'),
+                Menu::new()
+                    ->addIfCan('view-taxes', Link::toRoute('purchase.index', __('Taxes'))->addClass(route_is(['taxes.*']) ? 'active' : ''))
+                    ->addIfCan('view-expenses', Link::toRoute('expenses.index', __('Expenses'))->addClass(route_is(['expenses.*']) ? 'active' : ''))
+                    ->addIfCan('view-estimates', Link::toRoute('estimates.index', __('Estimates'))->addClass(route_is(['estimates.*']) ? 'active' : ''))
+                    ->addIfCan('view-invoices', Link::toRoute('invoices.index', __('Invoices'))->addClass(route_is(['invoices.*']) ? 'active' : ''))
+                    ->addParentClass('submenu')
+            );
+        }
+
         // Sales
         if(auth()->user()->canAny(['view-taxes','view-expenses','view-estimates','view-invoices'])){
             $activeClass = route_is(["taxes.*","expenses.*","estimates.*","invoices.*"]) ? "active" : "";
